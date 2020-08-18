697d019d3646363c283cb14dcb1be866bfdbe18b
sametweb <sametmutevellioglu@gmail.com>
2020-08-18 11:35:54 -0700

    MVP!!!!!!!!!!

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,12 +2,20 @@
 
 import sys
 
+
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.pc = 0
+        self.reg = [0] * 10
+        self.ram = [0] * 256
+        self.cmds = {
+            0b10000010: "LDI",
+            0b01000111: "PRN",
+            0b00000001: "HLT"
+        }
 
     def load(self):
         """Load a program into memory."""
@@ -18,27 +26,32 @@ class CPU:
 
         program = [
             # From print8.ls8
-            0b10000010, # LDI R0,8
+            0b10000010,  # LDI R0,8
             0b00000000,
             0b00001000,
-            0b01000111, # PRN R0
+            0b01000111,  # PRN R0
             0b00000000,
-            0b00000001, # HLT
+            0b00000001,  # HLT
         ]
 
         for instruction in program:
             self.ram[address] = instruction
             address += 1
 
-
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        # elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
+
+    def ram_read(self, pc):
+        return self.ram[pc]
+
+    def ram_write(self):
+        pass
 
     def trace(self):
         """
@@ -48,8 +61,8 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            #self.fl,
-            #self.ie,
+            # self.fl,
+            # self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
@@ -61,5 +74,30 @@ class CPU:
         print()
 
     def run(self):
-        """Run the CPU."""
-        pass
+        self.load()
+
+        running = True
+
+        while running:
+            cmd = self.ram[self.pc]
+            op_size = (cmd >> 6) + 1
+
+            if self.cmds[cmd] == 'LDI':
+                reg_index = self.ram[self.pc + 1]
+                num = self.ram[self.pc + 2]
+                self.reg[reg_index] = num
+
+            elif self.cmds[cmd] == 'PRN':
+                reg_index = self.ram[self.pc + 1]
+                num = self.reg[reg_index]
+                print(num)
+
+            elif self.cmds[cmd] == 'HLT':
+                running = False
+
+            self.pc += op_size
+
+
+# cpu = CPU()
+
+# cpu.run() IT FUCKING WORKS!
